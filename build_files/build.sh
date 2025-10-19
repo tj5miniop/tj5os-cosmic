c05136d84fcfa9c043a285d7e77c91770a8d8e53
tj5miniop <82961157+tj5miniop@users.noreply.github.com>
2025-10-19 10:53:06 +0100

    Add Nix Package Manager as an alternate pkg manager

--- build_files/build.sh
+++ build_files/build.sh
@@ -35,15 +35,6 @@ dnf5 -y install kora-icon-theme
 dnf5 -y copr disable hazel-bunny/ricing
 
 
-# Install dra for BUILD PROCESS ONLY
-mkdir -p /tmp/tj5os-build/
-cd /tmp/tj5os-build
-# Grab Latest Release from devmatteini/dra
-curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/devmatteini/dra/refs/heads/main/install.sh | bash -s -- --to .
-./dra download --select "brave-browser-{tag}-1.x86_64.rpm" brave/brave-browser
-dnf5 -y install ./*.rpm 
-
-
 # Install bazaar and other apps from ublue 
 dnf5 -y copr enable ublue-os/packages
 dnf5 -y install bazaar 
@@ -53,6 +44,8 @@ dnf5 -y copr enable xxmitsu/mesa-git
 dnf5 -y update
 dnf5 -y copr disable xxmitsu/mesa-git
 
+
+# Install Flatpak
 dnf5 -y install flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
@@ -61,23 +54,14 @@ dnf5 -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release
 dnf5 -y config-manager setopt fedora-cisco-openh264.enabled=1
 dnf5 -y install steam gamescope
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+# install Nix package manager 
+sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --daemon
 
 #Dotfiles installation and configurations (will only be used if no .config files for the respecitve apps are in the users home directory)
 #mkdir /usr/share/backgrounds/tj5os-cosmic/43
 #wget -O {wallpaper-name).{fileextenstion} https://raw.githubusercontent.com/pollux78/linuxnext-wallpapers/refs/heads/main/Games/20241128_010441.jpg
 
-
-# Cleanup
-cd / 
-rm -rf /tmp/tj5os-build
-
-
 #### Example for enabling a System Unit File
-
+systemctl enable nix-daemon.service
 systemctl enable podman.socket
+
